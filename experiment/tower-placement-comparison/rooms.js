08623a57c491c95fddb0f3b58e376cc7800bdd9c
Harabi <71678452+sy-harabi@users.noreply.github.com>
2026-09-14 18:52:43 +0900

    experiment: add tower comparison room seeds

---
+++ experiment/tower-placement-comparison/rooms.js
@@ -0,0 +1,7 @@
+// Representative shardSeason rooms for Prev / Next / Random navigation.
+// Any valid room name can still be entered manually; room data is fetched on demand.
+const CONTROLLER_ROOM_NAMES=[
+  "E27N37","W13N55","E4N27","E28N36","E27N8","E9N56","E18N77",
+  "W35N22","E28N27","E3N22","E27N15","W48N15","E5N57","E8N39",
+  "W24N13","W16N61","E34N22","E7N37","E2N6"
+];
