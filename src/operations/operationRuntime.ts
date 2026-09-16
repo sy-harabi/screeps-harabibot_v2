4ea33aa07c181db31222913fa9aacfe98132f9bd
이승열 <develten@naver.com>
2026-09-16 21:33:11 +0900

    operation

---
+++ src/operations/operationRuntime.ts
@@ -0,0 +1,46 @@
+// operationRuntime.ts
+
+const operationHeap = new Map<string, unknown>();
+
+let tempTick = -1;
+const operationTemp = new Map<string, unknown>();
+
+export function getOperationHeap<T extends object>(
+  operationId: string,
+): Partial<T> {
+  let heap = operationHeap.get(operationId);
+
+  if (heap === undefined) {
+    heap = {};
+    operationHeap.set(operationId, heap);
+  }
+
+  return heap as Partial<T>;
+}
+
+export function getOperationTemp<T extends object>(
+  operationId: string,
+): Partial<T> {
+  prepareTemp();
+
+  let temp = operationTemp.get(operationId);
+
+  if (temp === undefined) {
+    temp = {};
+    operationTemp.set(operationId, temp);
+  }
+
+  return temp as Partial<T>;
+}
+
+function prepareTemp(): void {
+  if (tempTick === Game.time) return;
+
+  tempTick = Game.time;
+  operationTemp.clear();
+}
+
+export function clearOperationRuntime(operationId: string): void {
+  operationHeap.delete(operationId);
+  operationTemp.delete(operationId);
+}
