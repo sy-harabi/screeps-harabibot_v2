6e45062c69240a6c186eb834b5b866441862e3ee
sy-harabi <lsy920118@gmail.com>
2026-09-18 14:31:36 +0900

    movemenetRumtime

--- src/capabilities/movement/movementRuntime.ts
+++ src/capabilities/movement/movementRuntime.ts
@@ -1,6 +1,13 @@
+import { getCreepHeap } from "../../runtime/creepRuntime"
+
 interface CreepMovementRuntime {
   cachedPath?: readonly RoomPosition[]
   nextPathIndex?: number
   goalKey?: string
   lastObservedPosition?: RoomPosition
+}
+
+export function getMovementRuntime(creepName: string): CreepMovementRuntime {
+  const heap = getCreepHeap(creepName)
+  return (heap._movement ??= {})
 }
