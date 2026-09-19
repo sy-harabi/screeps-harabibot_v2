91411ac7a4e2c57400d5f9afff3db28de05826f5
이승열 <develten@naver.com>
2026-09-20 00:51:26 +0900

    leadTime

--- src/operations/harvest/harvestOperation.ts
+++ src/operations/harvest/harvestOperation.ts
@@ -99,8 +99,12 @@ export const harvestOperationHandler: OperationHandler<HarvestOperationRecord> =
         continue
       }
 
-      sourceState.harvestPower += miner.getActiveBodyparts(WORK) * HARVEST_POWER
-      sourceState.numMiners++
+      const replacementLeadTime = miner.body.length * CREEP_SPAWN_TIME + sourceState.data.path.length
+
+      if (miner.ticksToLive ?? CREEP_LIFE_TIME > replacementLeadTime) {
+        sourceState.harvestPower += miner.getActiveBodyparts(WORK) * HARVEST_POWER
+        sourceState.numMiners++
+      }
     }
 
     for (const sourceId of sourceOrder) {
