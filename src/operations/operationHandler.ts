b1705639fdbc527828f010509e27c1e597d887a5
Harabi <71678452+sy-harabi@users.noreply.github.com>
2026-09-21 15:51:37 +0900

    Remove operation framework

--- src/operations/operationHandler.ts
+++
@@ -1,7 +0,0 @@
-import type { TickContext } from "../kernel/tickContext"
-import type { OperationRecord } from "./operation"
-
-export interface OperationHandler<T extends OperationRecord> {
-  plan?(operation: T, context: TickContext): void
-  execute?(operation: T, context: TickContext): void
-}
