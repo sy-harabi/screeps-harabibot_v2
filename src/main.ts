aa9eed8378d302c64f4c85115a54de7b3d7af851
Harabi <71678452+sy-harabi@users.noreply.github.com>
2026-09-22 16:49:24 +0900

    Run scheduled runtime cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { getRoomCostMatrix } from "./capabilities/movement/roomCostMatrix"
+import { getBaseRoomCostMatrix } from "./capabilities/movement/roomCostMatrix"
 import { run as runTraffic } from "./capabilities/movement/traffic"
 import { allocateSpawns } from "./capabilities/spawning/spawnAllocator"
 import { runColonies } from "./colony/colonyManager"
 import "./console/consoleApi"
 import { createTickContext } from "./kernel/tickContext"
 import { segmentManager } from "./persistence/segmentManager"
+import { runtimeRegistry } from "./runtime/runtimeRegistry"
 import "./visuals/roomVisual"
 
 export function loop(): void {
@@ -17,8 +18,9 @@ export function loop(): void {
   allocateSpawns()
 
   for (const room of Object.values(Game.rooms)) {
-    runTraffic(room, () => getRoomCostMatrix(room.name))
+    runTraffic(room, () => getBaseRoomCostMatrix(room.name))
   }
 
+  runtimeRegistry.cleanup()
   segmentManager.endTick()
 }
