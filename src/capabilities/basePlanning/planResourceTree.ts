9c4a0bcf75da4ff686b37f017a60b7d239732afc
Harabi <71678452+sy-harabi@users.noreply.github.com>
2026-09-22 15:56:23 +0900

    Use type-only imports in resource tree planner

--- src/capabilities/basePlanning/planResourceTree.ts
+++ src/capabilities/basePlanning/planResourceTree.ts
@@ -1,7 +1,7 @@
-import { RoomCoordinate } from "../../world/map/roomCoordinate"
+import type { RoomCoordinate } from "../../world/map/roomCoordinate"
 import { fromRoomIndex, isInsideRoom, NEIGHBOR_OFFSETS, ROOM_AREA, toRoomIndex } from "../../world/map/roomGrid"
-import { ControllerAreaCandidate } from "./findControllerAreaCandidates"
-import { CorePlan } from "./findCorePlans"
+import type { ControllerAreaCandidate } from "./findControllerAreaCandidates"
+import type { CorePlan } from "./findCorePlans"
 import { planResourceEndpoints } from "./planResourceEndpoints"
 import { buildResourceDistanceMap, getResourceRoadCost } from "./resourcePlanningUtils"
 
